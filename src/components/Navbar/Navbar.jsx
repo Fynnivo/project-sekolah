d97912942b982ba9805608745716f965612d9426
Fynnivo <ikhwandany6@gmail.com>
2022-12-18 13:18:27 +0700

    second commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,159 +1,71 @@
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import ikhwanLogo from '../../asset/ikhwan-logo.png'
-import ikhwan from '../../asset/20210918_123844-picsay.jpg'
+import ikhwanLogo from "../../asset/ikhwan-logo.png";
+import { Navbar } from "flowbite-react";
 
-
-const navigation = [
-  { name: 'Home', href: '#', current: true },
-  { name: 'Price', href: '#', current: false },
-  { name: 'Promo', href: '#', current: false },
-  { name: 'About', href: '#', current: false },
-]
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
-
-export default function Example() {
+const NavbarComponent = () => {
   return (
-    <Disclosure as="nav" className="bg-white  drop-shadow-sm">
-      {({ open }) => (
-        <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex transition-all items-center justify-center rounded-md p-2 text-black hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
-              </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 items-center">
-                  <img
-                    className="block h-8 w-auto lg:hidden"
-                    src={ikhwanLogo}
-                    alt="Your Company"
-                  />
-                  <img
-                    className="hidden h-8 w-auto lg:block"
-                    src={ikhwanLogo}
-                    alt="Your Company"
-                  />
-                </div>
-                <div className="hidden sm:ml-6  sm:block">
-                  <div className="flex space-x-4">
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current ? 'bg-teal-300 text-black' : 'text-black hover:bg-teal-300 hover:text-black',
-                          'px-3 py-2 rounded-md text-sm font-medium'
-                        )}
-                        aria-current={item.current ? 'page' : undefined}
-                      >
-                        {item.name}
-                      </a>
-                    ))}
-                  </div>
-                </div>
-              </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
-                  className="rounded-full bg-gray-300 p-1 text-gray-400 hover:text-black focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                >
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
+    <div className="sticky top-0 bg-white">
+      <Navbar fluid={true} rounded={true} className="bg-white">
+        <Navbar.Brand
+          href="/"
+          className="md:ml-[8rem] lg:ml-[8rem] ml-[2.6rem]"
+        >
+          <img
+            src={ikhwanLogo}
+            className="mr-3 h-6 sm:h-9"
+            alt="Flowbite Logo"
+          />
+          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+            Ikhwan
+          </span>
+        </Navbar.Brand>
+        <Navbar.Toggle className="" />
+        <Navbar.Collapse className="md:mr-[8rem] lg:mr-[8rem] mr-0">
+          <a
+            className="md:mb-0 text-center lg:mb-0 mb-2 pt-1.5 md:hover:border-b-2 hover:text-[#8b5cf6] border-[#8b5cf6]"
+            href="/navbars"
+          >
+            Home
+          </a>
+          <a
+            className="md:mb-0 text-center lg:mb-0 mb-2 pt-1.5 md:hover:border-b-2 hover:text-[#8b5cf6] border-[#8b5cf6]"
+            href="/navbars"
+          >
+            About
+          </a>
+          <a
+            className="md:mb-0 text-center lg:mb-0 mb-2 pt-1.5 md:hover:border-b-2 hover:text-[#8b5cf6] border-[#8b5cf6]"
+            href="/navbars"
+          >
+            Services
+          </a>
+          <a
+            className="md:mb-0 text-center lg:mb-0 mb-2 pt-1.5 md:hover:border-b-2 hover:text-[#8b5cf6] border-[#8b5cf6]"
+            href="/navbars"
+          >
+            Pricing
+          </a>
+          <a
+            className="md:mb-0 text-center lg:mb-0 mb-2 pt-1.5 md:hover:border-b-2 hover:text-[#8b5cf6] border-[#8b5cf6]"
+            href="/navbars"
+          >
+            Contact
+          </a>
+          <a
+            className="bg-[#8b5cf6] text-white text-center md:mb-0 lg:mb-0 mb-2 px-5 py-1 pt-1.5 rounded-full"
+            href="/"
+          >
+            Sign Up
+          </a>
+          <a
+            className="border-2 border-[#8b5cf6] px-5 py-1 text-center rounded-full "
+            href="/navbars"
+          >
+            Sign In
+          </a>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+};
 
-                {/* Profile dropdown */}
-                <Menu as="div" className="relative ml-3">
-                  <div>
-                    <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                      <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src={ikhwan}
-                        alt=""
-                      />
-                    </Menu.Button>
-                  </div>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="absolute right-0 z-20 mt-2 w-48 origin-top-right rounded-md bg-gray-500 py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <button  
-                            to="#"
-                            className={classNames(active ? 'text-gray-800' : '', 'block w-full px-4 text-start py-2 text-sm text-white')}
-                          >
-                            Your Profile 
-                          </button>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <button
-                            to="#"
-                            className={classNames(active ? 'text-gray-800' : '', 'block w-full px-4 py-2 text-sm text-start text-white')}
-                          >
-                            Settings
-                          </button>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <button  
-                            to="#"
-                            className={classNames(active ? 'text-gray-800' : '', 'block w-full px-4 py-2 text-sm text-start text-white')}
-                          >
-                            Sign out
-                          </button>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
-              </div>
-            </div>
-          </div>
-
-          <Disclosure.Panel className="sm:hidden md:hidden">
-            <div className="space-y-1 px-2 pt-2 pb-3 transition ease-in duration-200">
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-black hover:bg-gray-900 hover:text-white',
-                    'block px-3 py-2 rounded-md text-base font-medium'
-                  )}
-                  aria-current={item.current ? 'page' : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
-            </div>
-          </Disclosure.Panel>
-        </>
-      )}
-    </Disclosure>
-  )
-}
-
+export default NavbarComponent;
